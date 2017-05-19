e02cd96149869d934abcadc5c76a8981b2c466a1
Cory Johnson <cjohnson@brevium.com>
2017-05-19 17:06:22 -0600

    Prepping Production build, work in progress

---
+++ webpack.config.prod.js
@@ -0,0 +1,30 @@
+import path from 'path';
+import webpack from 'webpack'
+
+export default {
+  debug: true,
+  devtool: 'source-map',
+  noInfo: false,
+  entry: [
+    //add hot reloading middleware here??
+    path.resolve(__dirname, 'src/index')
+  ],
+  target: 'web',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  plugins: [
+    //Elliminate duplicate packages when generating bundle
+    new webpack.optimize.DedupePlugin(),
+    //Minify JS
+    new webpack.optimize.UglifyJsPlugin()
+  ],
+  module: {
+    loaders: [
+      {test: /\.js$/, exclude: /node_modules/, loaders: ['babel']},
+      {test: /\.css$/, loaders: ['style','css']}
+    ]
+  }
+}
